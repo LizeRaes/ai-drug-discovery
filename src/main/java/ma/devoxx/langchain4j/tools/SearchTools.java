36204cf8eb62612c4175d1504ece26f3d7eeb4ee
Mohamed <mohamed.aitabderrahman@gmail.com>
2024-09-27 20:55:24 +0200

    Add query compressor

--- src/main/java/ma/devoxx/langchain4j/tools/SearchTools.java
+++ src/main/java/ma/devoxx/langchain4j/tools/SearchTools.java
@@ -1,24 +1,27 @@
 package ma.devoxx.langchain4j.tools;
 
 import ma.devoxx.langchain4j.dbs.PublicProteinDatabase;
-import org.jboss.logging.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 class SearchTools {
 
+    private static final Logger logger = LoggerFactory.getLogger(SearchTools.class);
+
     static String findCDRsForAntibody(String antibodyName) {
         // TODO should move to SQLRetriever, no need to use tools for this one
-        Logger.getLogger(SearchTools.class).info("Called findCDRsForAntibody() with antibodyName='" + antibodyName + "'");
+        logger.info("Called findCDRsForAntibody() with antibodyName='{}'", antibodyName);
         PublicProteinDatabase db = new PublicProteinDatabase();
         String cdrs = db.retrieveSequences(antibodyName);
-        Logger.getLogger(SearchTools.class).info("Found " + cdrs);
+        logger.info("CDR(s) Found {}", cdrs);
         return cdrs;
     }
 
     static String findSequenceForAntigen(String antigenName) {
-        Logger.getLogger(SearchTools.class).info("Called findSequenceForAntigen() with antigenName='" + antigenName + "'");
+        logger.info("Called findSequenceForAntigen() with antigenName='{}'", antigenName);
         PublicProteinDatabase db = new PublicProteinDatabase();
         String cdrs = db.retrieveSequences(antigenName);
-        Logger.getLogger(SearchTools.class).info("Found " + cdrs);
+        logger.info("Found {}", cdrs);
         return cdrs;
     }
 }
