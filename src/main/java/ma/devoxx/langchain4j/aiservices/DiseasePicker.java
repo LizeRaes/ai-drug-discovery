d96500d524da4568f5bbc52bddb24e4102d800e1
Mohamed <mohamed.aitabderrahman@gmail.com>
2024-09-29 00:01:52 +0200

    Demo

--- src/main/java/ma/devoxx/langchain4j/aiservices/DiseasePicker.java
+++ src/main/java/ma/devoxx/langchain4j/aiservices/DiseasePicker.java
@@ -5,12 +5,17 @@ import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.TokenStream;
 import dev.langchain4j.service.UserMessage;
 import io.quarkiverse.langchain4j.RegisterAiService;
+import io.quarkiverse.langchain4j.guardrails.InputGuardrails;
 import jakarta.enterprise.context.ApplicationScoped;
+import ma.devoxx.langchain4j.guardrails.NoBiologicalWeapon;
 import ma.devoxx.langchain4j.tools.ToolsForDiseasePicker;
 
 @ApplicationScoped
 @RegisterAiService(tools = ToolsForDiseasePicker.class)
 public interface DiseasePicker {
+
+    //@GUARDRAILS
+    //@InputGuardrails(NoBiologicalWeapon.class)
     @SystemMessage("""
     You are a helpful antibody drug research assistant. If the user asks, help them to choose the best disease for antibody research based on their questions.
     Only once the user chooses a disease to move forward with, you call storeDiseaseName with the disease name, and inform the user that their disease was stored and if they want to move on to the next step.
