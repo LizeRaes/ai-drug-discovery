09b8e2c5f385662e969d2dc13bcd9ff23a944681
Mohamed <mohamed.aitabderrahman@gmail.com>
2024-09-29 21:04:42 +0200

    Comment audio api

--- src/main/java/ma/devoxx/langchain4j/web/rest/SpeechToTextResource.java
+++ src/main/java/ma/devoxx/langchain4j/web/rest/SpeechToTextResource.java
@@ -13,26 +13,28 @@ public class SpeechToTextResource {
 
     Logger logger = Logger.getLogger(SpeechToTextResource.class);
 
-    private final String apiKey = System.getenv("GEMINI_TOKEN");
-    private static final String MODEL_NAME = "gemini-1.5-flash-001";
+    // @HERE AUDIO
+//    private final String apiKey = System.getenv("GEMINI_TOKEN");
+//    private static final String MODEL_NAME = "gemini-1.5-flash-001";
 
-    @POST
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getText(String base64) {
-        GoogleAiGeminiChatModel model = GoogleAiGeminiChatModel.builder()
-                .apiKey(apiKey)
-                .modelName(MODEL_NAME)
-                .logRequestsAndResponses(true)
-                .build();
 
-        var response = model.generate(
-                SystemMessage.from("repeat the text from the audio message"),
-                new UserMessage(AudioContent.from(base64, "audio/mp3")));
-
-        String message = response.content().text();
-
-        logger.info("audio to text = " + message);
-
-        return message;
-    }
+//    @POST
+//    @Produces(MediaType.TEXT_PLAIN)
+//    public String getText(String base64) {
+//        GoogleAiGeminiChatModel model = GoogleAiGeminiChatModel.builder()
+//                .apiKey(apiKey)
+//                .modelName(MODEL_NAME)
+//                .logRequestsAndResponses(true)
+//                .build();
+//
+//        var response = model.generate(
+//                SystemMessage.from("repeat the text from the audio message"),
+//                new UserMessage(AudioContent.from(base64, "audio/mp3")));
+//
+//        String message = response.content().text();
+//
+//        logger.info("audio to text = " + message);
+//
+//        return message;
+//    }
 }
