fd50c10ab427391e164b4fee0f49949c9bffaaba
Mohamed <mohamed.aitabderrahman@gmail.com>
2024-09-29 10:42:45 +0200

    Demo

--- src/main/java/ma/devoxx/langchain4j/aiservices/DiseasePicker.java
+++ src/main/java/ma/devoxx/langchain4j/aiservices/DiseasePicker.java
@@ -13,9 +13,7 @@ import ma.devoxx.langchain4j.tools.ToolsForDiseasePicker;
 @ApplicationScoped
 @RegisterAiService(tools = ToolsForDiseasePicker.class)
 public interface DiseasePicker {
-
-    //@GUARDRAILS
-    //@InputGuardrails(NoBiologicalWeapon.class)
+    
     @SystemMessage("""
     You are a helpful antibody drug research assistant. If the user asks, help them to choose the best disease for antibody research based on their questions.
     Only once the user chooses a disease to move forward with, you call storeDiseaseName with the disease name, and inform the user that their disease was stored and if they want to move on to the next step.
