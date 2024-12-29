09e4450e14019628cca419575ba74d3ad5774fb3
Mohamed <mohamed.aitabderrahman@gmail.com>
2024-12-29 10:11:36 +0100

    Polish Speech to Text API

--- src/main/java/ma/devoxx/langchain4j/web/rest/SpeechToTextResource.java
+++ src/main/java/ma/devoxx/langchain4j/web/rest/SpeechToTextResource.java
@@ -3,6 +3,7 @@ package ma.devoxx.langchain4j.web.rest;
 import dev.langchain4j.data.message.AudioContent;
 import dev.langchain4j.data.message.SystemMessage;
 import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
@@ -11,27 +12,27 @@ import org.jboss.logging.Logger;
 @Path("/audio")
 public class SpeechToTextResource {
 
-    Logger logger = Logger.getLogger(SpeechToTextResource.class);
-
-    private final String apiKey = System.getenv("GEMINI_TOKEN");
+    private static final Logger LOGGER = Logger.getLogger(SpeechToTextResource.class);
+    private static final String API_KEY = System.getenv("GEMINI_TOKEN");
     private static final String MODEL_NAME = "gemini-1.5-flash-001";
 
     @POST
     @Produces(MediaType.TEXT_PLAIN)
     public String getText(String base64) {
-        GoogleAiGeminiChatModel model = GoogleAiGeminiChatModel.builder()
-                .apiKey(apiKey)
+        ChatLanguageModel model = GoogleAiGeminiChatModel.builder()
+                .apiKey(API_KEY)
                 .modelName(MODEL_NAME)
                 .logRequestsAndResponses(true)
                 .build();
 
+        LOGGER.info("Gemini is converting audio to text...");
         var response = model.generate(
                 SystemMessage.from("repeat the text from the audio message"),
                 new UserMessage(AudioContent.from(base64, "audio/mp3")));
 
         String message = response.content().text();
 
-        logger.info("audio to text = " + message);
+        LOGGER.info("Audio convert. Text = " + message);
 
         return message;
     }
